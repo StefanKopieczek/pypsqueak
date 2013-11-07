463c7ff82b3fc0b2cdd67a4eb26e5ba8fd582796
Alex Lodge <Alexander.Lodge@metaswitch.com>
2013-11-07 20:17:37 +0000

    Test changes to data

--- letterstats.py
+++ letterstats.py
@@ -1,20 +1,20 @@
 import audiotools
 from collections import Counter
-from math import sqrt
+from math import sqrt, log10
 
-BANDS = [(40,   1000, 'LOW'),
+BANDS = [(40, 1000, 'LOW'),
          (1000, 4000, 'MEDIUM'),
          (4000, 20000, 'HIGH')]
-DEBUG = False 
+DEBUG = False
 
 def _dprint(text):
     global DEBUG
     if DEBUG: print text + '\n'
-         
-def get_best_match(samples, 
-                   sample_frequency, 
-                   training_stats, 
-                   rejection_threshold=None, # Todo
+
+def get_best_match(samples,
+                   sample_frequency,
+                   training_stats,
+                   rejection_threshold=None,  # Todo
                    k=3):
     fingerprint = analyse(samples, sample_frequency)
 
@@ -24,13 +24,13 @@ def get_best_match(samples,
         best_matches.append((stat['letter'], _get_distance(stat['analysis'], fingerprint)))
         best_matches.sort(key=lambda stat : stat[1])
         best_matches = best_matches[:k]
-    print best_matches 
+    print best_matches
 
     # Break ties by choosing closest neighbour.
     counter = Counter([match[0] for match in best_matches])
-    indices = {letterdata[0]:(k-revidx-1) 
+    indices = {letterdata[0]:(k - revidx - 1)
                             for revidx, letterdata in enumerate(best_matches[::-1])}
-    most_frequent_with_indices = {indices[key[0]]:key[0] 
+    most_frequent_with_indices = {indices[key[0]]:key[0]
                                                for key in counter.most_common()}
     return min(most_frequent_with_indices.iteritems())[1]
 
@@ -42,11 +42,11 @@ def analyse(samples, sample_frequency):
     n = len(samples)
     overall_energy = audiotools.get_energy(samples)
 
-    analysis = [n]
+    analysis = [1]
     _dprint('Analysing sample with length %d and energy %d' % (n, overall_energy))
     for ii in xrange(3):
         _dprint('\tParsing segment ' + str(ii))
-        analysis.extend(_analyse_segment(samples[int(ii*n/3.0):int((ii+1)*n/3.0)],
+        analysis.extend(_analyse_segment(samples[int(ii * n / 3.0):int((ii + 1) * n / 3.0)],
                                          sample_frequency,
                                          overall_energy))
 
@@ -66,10 +66,10 @@ def load_stats_from_file(fname):
     return stats
 
 def save_stats_to_file(stats, fname):
-    f = open(fname, 'w')
+    f = open(fname, 'a')
     _dprint("Saving to file.")
     for stat in stats:
-        line = (stat['letter'] + ' ' + 
+        line = (stat['letter'] + ' ' +
                 ' '.join([str(datum) for datum in stat['analysis']]) +
                 '\n')
         f.write(line)
@@ -81,30 +81,30 @@ def _get_distance(v1, v2):
     if len(v1) != len(v2):
         raise ValueError("Cannot get distance between vectors of different " +
                          "dimensions: %d and %d!" % (len(v1), len(v2)))
-    return sqrt(sum(((v1[idx]-v2[idx])**2 for idx in xrange(len(v1)))))
+    return sqrt(sum(((v1[idx] - v2[idx]) ** 2 for idx in xrange(len(v1)))))
 
 def _get_predominant_band(samples, sample_frequency):
     """Test function to make sure that we're detecting bands correctly.
        Slightly unfair, since not all bands are equal in size."""
-    levels = [(audiotools.get_energy_in_range(samples, 
-                                              band[0], 
-                                              band[1], 
-                                              sample_frequency), 
+    levels = [(audiotools.get_energy_in_range(samples,
+                                              band[0],
+                                              band[1],
+                                              sample_frequency),
                band) for band in BANDS]
-    return max(levels, key = lambda level:level[0])[1][2]
+    return max(levels, key=lambda level:level[0])[1][2]
 
 
 def _analyse_segment(samples, sample_frequency, overall_energy):
     analysis = []
     energy = audiotools.get_energy(samples)
-    analysis.append(energy * 1.0 / overall_energy)
+    analysis.append(log10(energy) * 1.0 / log10(overall_energy))
     for band in BANDS:
         _dprint('\t\tAnalysing band ' + str(band))
-        band_energy = audiotools.get_energy_in_range(samples, 
-                                                     band[0], 
-                                                     band[1], 
+        band_energy = audiotools.get_energy_in_range(samples,
+                                                     band[0],
+                                                     band[1],
                                                      sample_frequency)
-        analysis.append(band_energy * 1.0 / overall_energy)
+        analysis.append(log10(band_energy) * 1.0 / log10(overall_energy))
 
     return analysis
 
--- peaklistener.py
+++ peaklistener.py
@@ -5,36 +5,36 @@ import threading
 from time import sleep, time # LOL FIONA ROX!!!1!1!!
 import audiotools, letterstats
 
-CHUNK = 1024 
+CHUNK = 1024
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 44100
 MIN_PEAK_DURATION = 0.500
-DEBUG = False 
+DEBUG = True
 
 def _dprint(text):
     global DEBUG
     if DEBUG: print text + '\n'
 
-def get_shorts_from_bytes(data):       
+def get_shorts_from_bytes(data):
     """Converts a string of byte values into an array of (short) int values,
        assuming Little Endian format."""
-    shorts = []    
+    shorts = []
     for idx in xrange(0, len(data), 2):
-        shorts.append(struct.unpack('<h', data[idx] + data[idx+1])[0])    
+        shorts.append(struct.unpack('<h', data[idx] + data[idx + 1])[0])
     return shorts
 
 
-class PeakListener(object):    
+class PeakListener(object):
     """The PeakListener listens on the computer's default input device, and
        listens for any periods of audio that are louder than a certain
        threshold.
        When a peak has ended, it calls the peak_data_handler it was given when
        constructed, passing the sample data as a list of ints."""
 
-    def __init__(self, peak_data_handler, low_pass=800, high_pass=2500):        
+    def __init__(self, peak_data_handler, low_pass=800, high_pass=2500):
         # Are we currently in a peak?
-        self._is_high = False 
+        self._is_high = False
 
         # Thread used by the mic listener.
         self._mic_thread = MicInputThread(lambda data: self._handle_input(data))
@@ -50,56 +50,56 @@ class PeakListener(object):
 
         # The value at which we consider a peak to have ended.
         # Will need tweaking for a given mic.
-        self._low_pass = low_pass 
+        self._low_pass = low_pass
 
         # The value at which we consider a peak to have started.
         # Will need tweaking for a given mic.
-        self._high_pass = high_pass 
-        
+        self._high_pass = high_pass
+
     def start(self):
         """Start listening to input."""
         _dprint("Peak Listener starts.")
         self._mic_thread.start()
-        
+
     def stop(self):
         """Stop listening to input, and clean up all state."""
         self._mic_thread.stop()
         self._buffer = []
         self._last_peak_time = 0
         _dprint("Peak Listener stopped.")
-        
+
     def _handle_input(self, data):
         """Internal method to handle a new chunk of audio from the mic."""
         # Carry out all new processing in a new thread.
-        # This stops us dragging the microphone thread, which should be kept 
-        # free.        
-        threading.Thread(target=self._handle_input_in_thread, 
+        # This stops us dragging the microphone thread, which should be kept
+        # free.
+        threading.Thread(target=self._handle_input_in_thread,
                          args=(data,)).start()
 
-    def _handle_input_in_thread(self, data):                              
+    def _handle_input_in_thread(self, data):
         """Internal method to handle a new chunk of audio from the mic.
            Should be called in its own thread to not block the input thread."""
-        samples = get_shorts_from_bytes(data)     
-        energy = audiotools.get_energy(samples) 
-        time_since_last_peak = time() - self._last_peak_time            
+        samples = get_shorts_from_bytes(data)
+        energy = audiotools.get_energy(samples)
+        time_since_last_peak = time() - self._last_peak_time
         if self._is_high:
             # We're in a peak, so record off the latest audio.
             self._buffer.extend(samples)
-        if (energy < self._low_pass and 
-            self._is_high and 
-            time_since_last_peak > MIN_PEAK_DURATION):            
+        if (energy < self._low_pass and
+            self._is_high and
+            time_since_last_peak > MIN_PEAK_DURATION):
             # We're in a peak, but it's just ended. Handle the audio.
             _dprint("Peak ends.")
-            self._is_high = False         
-            self._peak_data_handler(self._buffer[:]) # Shallow copy
+            self._is_high = False
+            self._peak_data_handler(self._buffer[:])  # Shallow copy
             self._buffer = []
         elif (energy > self._high_pass and not self._is_high):
             # We weren't in a peak, but one just started. Begin recording.
             _dprint("Peak starts.")
             self._is_high = True
-            last_peak_time = time()         
+            last_peak_time = time()
             self._buffer = []
-                    
+
 class MicInputThread(threading.Thread):
     """Thread used to listen to mic input, and pass on each sample to a given
        callback function."""
@@ -108,34 +108,34 @@ class MicInputThread(threading.Thread):
         self._is_running = True
         self._new_sample_callback = new_sample_callback
         threading.Thread.__init__(self)
-        
+
     def stop(self):
         """Terminate the listener thread."""
         self._is_running = False
-        
-    def run(self):        
+
+    def run(self):
         """Overrides Thread.run()."""
-        global FORMAT   # Bitness/endianism of audio samples.
-        global RATE     # Sample rate in samples/sec. 
-        global CHANNELS # Num channels
-        global CHUNK    # Samples to get in one go.
+        global FORMAT  # Bitness/endianism of audio samples.
+        global RATE  # Sample rate in samples/sec.
+        global CHANNELS  # Num channels
+        global CHUNK  # Samples to get in one go.
         p = pyaudio.PyAudio()
 
         stream = p.open(format=FORMAT,
                         channels=CHANNELS,
                         rate=RATE,
                         input=True,
-                        frames_per_buffer=CHUNK)                                
+                        frames_per_buffer=CHUNK)
         print "MicInputThread begins listening."
-        
+
         try:
             while self._is_running:
                 data = []
                 data.extend(stream.read(CHUNK))
-                if self._new_sample_callback:                
+                if self._new_sample_callback:
                     self._new_sample_callback(data)
         finally:
-            # We've ended or crashed. Either way, tidy up. 
+            # We've ended or crashed. Either way, tidy up.
             stream.stop_stream()
             stream.close()
             p.terminate()
@@ -143,8 +143,8 @@ class MicInputThread(threading.Thread):
 def handle_peak(samples):
     """Demo function to be called when we get a peak.
        Put something awesome here."""
-    print "Got peak!" 
-    
-if __name__=="__main__":
+    print "Got peak!"
+
+if __name__ == "__main__":
     listener = PeakListener(handle_peak)
     listener.start()

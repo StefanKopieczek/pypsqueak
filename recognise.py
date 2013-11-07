0737ed8819aa41cdc858165aae930b58880b8b1e
Rolandas Glotnis <rolandas.glotnis@metaswitch.com>
2013-11-07 23:35:03 +0000

    integration with windows speech sdk

--- recognise.py
+++ recognise.py
@@ -2,6 +2,7 @@ import letterstats
 import sys
 from peaklistener import PeakListener, RATE
 from time import sleep
+import speech
 
 if __name__ == "__main__":
     fname = sys.argv[1]
@@ -10,9 +11,15 @@ if __name__ == "__main__":
     def handle_peak(samples):
         global RATE
         global stats
-        print "Processing input"
-        best_match = letterstats.get_best_match(samples, RATE, stats) 
-        print "I think you said '%s'." % best_match 
+        """
+        http://www.renuncln.com/blog/2012/05/28/python-speech-recognition-and-tts-part-i-pyspeech/
+        """
+        spoken = speech.input("Processing input")
+        print "I think you said '%s'." % spoken
+        #print "Processing input"
+        #best_match = letterstats.get_best_match(samples, RATE, stats) 
+        #print "I think you said '%s'." % best_match 
+    
     
     listener = PeakListener(handle_peak)
     listener.start()
